2ff4fdde86b498d4d2f4ddb4fd0981b086dc11d8
Murari Gupta <alexgupta2367@gmail.com>
2023-08-09 22:00:53 +0545

    added customerror and errorHandler files

---
+++ server/src/errors/CustomError.js
@@ -0,0 +1,27 @@
+class CustomError extends Error {
+  httpStatusCode;
+  timestamp;
+  documentationUrl;
+
+  constructor(httpStatusCode, message, documentationUrl) {
+    if (message) {
+      super(message);
+    } else {
+      super('A generic error occurred!');
+    }
+
+    // initializing the class properties
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+    this.httpStatusCode = httpStatusCode;
+    this.timestamp = new Date().toISOString();
+    this.documentationUrl = documentationUrl;
+
+    // attaching a call stack to the current class,
+    // preventing the constructor call to appear in the stack trace
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+const CustomHttpError = CustomError;
+
+export { CustomHttpError };
--- server/src/middleware/errorHandler.js
+++ server/src/middleware/errorHandler.js
@@ -1,26 +1,71 @@
+import config from '../configs/config.js';
 import statusCodes from '../constants/statusCodes.js';
+import { CustomHttpError } from '../errors/CustomError.js';
 
 const errorHandler = (err, req, res, next) => {
-  console.log('I enterd into the error handler middleware');
   const defaultError = {
-    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    httpStatusCode: err.statusCode || statusCodes.INTERNAL_SERVER_ERROR,
     msg: err.message || 'Something went wrong, try again later',
   };
-  if (err.name === 'ValidationError') {
-    defaultError.statusCode = StatusCodes.BAD_REQUEST;
-    // defaultError.msg = err.message
-    defaultError.msg = Object.values(err.errors)
-      .map((item) => item.message)
-      .join(',');
-  }
-  if (err.code && err.code === 11000) {
-    defaultError.statusCode = StatusCodes.BAD_REQUEST;
-    defaultError.msg = `${Object.keys(err.keyValue)} field has to be unique`;
+
+  // if the error is a custom defined error
+  if (err instanceof CustomHttpError) {
+    defaultError.httpStatusCode = err.httpStatusCode;
+    defaultError.msg = err.message;
+  } else {
+    // hide the detailed error message in production
+    // for security reasons
+    if (config.node_env !== 'production') {
+      // since in JavaScript you can also
+      // directly throw strings
+      if (typeof err === 'string') {
+        message = err;
+      } else if (err instanceof Error) {
+        message = err.message;
+      }
+    }
   }
 
-  console.log({ defaultError });
+  let stackTrace = undefined;
 
-  res.status(defaultError.statusCode).send({ msg: defaultError.msg });
+  // return the stack trace only when
+  // developing locally or in stage
+  if (config.node_env !== 'production') {
+    stackTrace = err.stack;
+  }
+
+  // logg the error
+  console.error(err);
+  // other custom behaviors...
+
+  // return the standard error response
+  res.status(httpStatusCode).send({
+    error: {
+      message: message,
+      timestamp: err.timestamp || undefined,
+      documentationUrl: err.documentationUrl || undefined,
+      stackTrace: stackTrace,
+    },
+  });
+
+  return next(err);
+
+  // Handle specific error types
+  // if (err.name === 'ValidationError') {
+  //   defaultError.statusCode = statusCodes.BAD_REQUEST;
+  //   // defaultError.msg = err.message
+  //   defaultError.msg = Object.values(err.errors)
+  //     .map((item) => item.message)
+  //     .join(',');
+  // }
+  // if (err.code && err.code === 11000) {
+  //   defaultError.statusCode = statusCodes.BAD_REQUEST;
+  //   defaultError.msg = `${Object.keys(err.keyValue)} field has to be unique`;
+  // }
+
+  // console.log({ defaultError });
+
+  // res.status(defaultError.statusCode).send({ msg: defaultError.msg });
 };
 
 export default errorHandler;
