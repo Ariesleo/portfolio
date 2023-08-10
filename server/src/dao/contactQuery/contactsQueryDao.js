cc2a38a4279c49b74aeec06cc382132cb07b01a6
Murari Gupta <alexgupta2367@gmail.com>
2023-08-10 17:45:45 +0545

    removing all customhttperror and added applicationerror handling techniques

--- server/src/dao/contactQuery/contactsQueryDao.js
+++ server/src/dao/contactQuery/contactsQueryDao.js
@@ -1,13 +1,14 @@
 import statusCodes from '../../constants/statusCodes.js';
-import { CustomHttpError } from '../../errors/CustomError.js';
+import { CommonError } from '../../lib/api/error/commonError.js';
 import { ContactQueryModel } from '../../models/contactsQuery.js';
+import { ApplicationError } from '../../lib/api/error/applicationError.js';
 
 const updateContact = async (data) => {
   try {
     const res = await ContactQueryModel.create(data);
     return res;
   } catch (err) {
-    throw new CustomHttpError(statusCodes.INTERNAL_SERVER_ERROR, err.message);
+    throw new ApplicationError(CommonError.INTERNAL_SERVER_ERROR);
   }
 };
 
--- server/src/services/contactQuery/contactsQuery.js
+++ server/src/services/contactQuery/contactsQuery.js
@@ -1,5 +1,6 @@
+import { CommonError } from '../../lib/api/error/commonError.js';
 import updateContact from '../../dao/contactQuery/contactsQueryDao.js';
-import { CustomHttpError } from '../../errors/CustomError.js';
+import { ApplicationError } from '../../lib/api/error/applicationError.js';
 
 const postContactQuery = async (dataQuery) => {
   const { name, email, message } = dataQuery;
@@ -7,8 +8,9 @@ const postContactQuery = async (dataQuery) => {
   try {
     const createContactQuery = await updateContact(dataQuery);
     return createContactQuery;
+    // throw new ApplicationError(CommonError.BAD_REQUEST);
   } catch (err) {
-    throw new CustomHttpError(err.httpStatusCode, err.message);
+    throw new ApplicationError(err);
   }
 };
 
