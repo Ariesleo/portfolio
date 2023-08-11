ea6f6a31c82af965d39141a9857409b8f42e3273
Murari Gupta <alexgupta2367@gmail.com>
2023-08-11 21:21:45 +0545

    added seperate yup validation for partial project

--- server/src/controllers/projects/validation.js
+++ server/src/controllers/projects/validation.js
@@ -25,4 +25,27 @@ const projectSchema = () => {
   });
 };
 
-export { projectSchema };
+// Validate the partial data or payload for the project
+const patchProjectSchema = () => {
+  return Yup.object().shape({
+    title: Yup.string()
+      .trim()
+      .min(3, 'Title must be at least 3 characters.')
+      .max(50, 'Title cannot exceed 50 characters.'),
+    description: Yup.string()
+      .trim()
+      .min(100, 'Description must be at least 100 characters.')
+      .max(1000, 'Description cannot exceed 1000 characters.'),
+    technologies: Yup.array()
+      .of(Yup.string())
+      .min(1, 'At least one technology is required.'),
+    image: Yup.string().nullable(),
+    githubUrl: Yup.string().url('Invalid URL format for GitHub.').nullable(),
+    demoUrl: Yup.string().url('Invalid URL format for demo.').nullable(),
+    isPersonalProject: Yup.boolean().default(false),
+    startDate: Yup.date().nullable(),
+    endDate: Yup.date().nullable(),
+  });
+};
+
+export { projectSchema, patchProjectSchema };
