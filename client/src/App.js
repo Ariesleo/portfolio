c200e895db8503e83188df634e9400e9744d9602
Murari Gupta <alexgupta2367@gmail.com>
2023-08-15 14:54:07 +0545

    added  proxy property to the package file for avoiding the cross origin problem during developmetn moving the card component to the atom folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,17 @@
+import Button from './components/atoms/button';
+
 function App() {
+  const clickEvent = () => {
+    console.log('hero hamda');
+  };
+
   return (
     <div>
-      <h2>Protfolio</h2>
+      <h3>Micheal Weaver</h3>
+      <p>hero hamda</p>
+      <Button onClick={clickEvent} outlined>
+        Skip
+      </Button>
     </div>
   );
 }
