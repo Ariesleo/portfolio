18059f93606567bab4a3b75da432e45015945891
Murari Gupta <alexgupta2367@gmail.com>
2023-08-18 14:43:25 +0545

    added teh bottom nav or the footer

--- client/src/pages/dashboard/sharedLayout/SharedLayout.js
+++ client/src/pages/dashboard/sharedLayout/SharedLayout.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import TopNav from '../../../components/molecules/navbar/TopNav';
+import BottomNav from '../../../components/molecules/navbar/BottomNav';
+import './SharedLayout.css'; // Import your CSS file for styling
 
 const SharedLayout = () => {
   return (
-    <>
+    <div className="shared-layout">
       <nav>
         <TopNav />
       </nav>
-      <div>
+      <div className="content">
         <Outlet />
       </div>
-    </>
+      <div className="footer">
+        <BottomNav />
+      </div>
+    </div>
   );
 };
 
