7d6bcd64ceb23e35a7bd8d142fcf0d8f95393d93
Murari Gupta <alexgupta2367@gmail.com>
2023-08-20 20:47:05 +0545

    worked on the about-me page

--- client/src/pages/dashboard/about-me/about-me.js
+++ client/src/pages/dashboard/about-me/about-me.js
@@ -1,7 +1,81 @@
 import React from 'react';
+import maskMan from '../../../images/files/maskMan.png';
+import { useAppContext } from '../../../context/appContext';
+import {
+  DescriptionAndImage,
+  Description,
+  Image,
+  Wrapper,
+  Quotation,
+  Skills,
+  SkillWrapper,
+  CurrentProjectStatus,
+} from './styles';
 
 const AboutMe = () => {
-  return <div>AboutMe</div>;
+  const profileData = useAppContext();
+  // console.log(Object.entries(profileData.skills));
+  // const skills = Object.entries(profileData.skills);
+
+  return (
+    <>
+      {Object.keys(profileData).length === 0 ? (
+        <p>...fetching data</p>
+      ) : (
+        <Wrapper>
+          <DescriptionAndImage>
+            {/* this section for description and image */}
+            <Description>
+              <p>{profileData.description}</p>
+            </Description>
+            <div>
+              <Image src={maskMan} alt="maskman" width="380px" height="340px" />
+              <CurrentProjectStatus>
+                <div></div>
+                <span>
+                  currently working on{' '}
+                  {profileData && profileData.currentProject ? (
+                    <a href={profileData.currentProject.projectUrl}>
+                      {profileData.currentProject.projectName}
+                    </a>
+                  ) : (
+                    <span>...loading</span>
+                  )}
+                </span>
+              </CurrentProjectStatus>
+            </div>
+          </DescriptionAndImage>
+          <Quotation>
+            {/* quotation */}
+            <q>
+              {profileData.quote.message}{' '}
+              <span>{profileData.quote.author}</span>
+            </q>
+          </Quotation>
+          <h4>#Skills----------</h4>
+          <Skills>
+            {/* skills */}
+            {profileData && profileData.skills ? (
+              <>
+                {Object.entries(profileData.skills).map((skill, index) => {
+                  return (
+                    <SkillWrapper key={index}>
+                      <h5>{skill[0]}</h5>
+                      {skill[1].map((data, id) => {
+                        return <p key={id}>{data}</p>;
+                      })}
+                    </SkillWrapper>
+                  );
+                })}
+              </>
+            ) : (
+              <p>No Skills to display</p>
+            )}
+          </Skills>
+        </Wrapper>
+      )}
+    </>
+  );
 };
 
 export default AboutMe;
---
+++ client/src/pages/dashboard/about-me/styles.js
@@ -0,0 +1,112 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  margin: 5% 4%;
+  color: var(--grey);
+
+  @media (max-width: 700px) {
+    margin: 0px 40px;
+  }
+`;
+
+const DescriptionAndImage = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-around;
+`;
+const Description = styled.div`
+  width: 50%;
+  color: var(--grey);
+  text-align: justify;
+
+  @media (max-width: 900px) {
+    /* Tablet and below */
+    width: auto;
+  }
+`;
+const Image = styled.img`
+  /* width: 50%; */
+  /* background-color: green; */
+  @media (max-width: 700px) {
+    width: 300px;
+    height: 280px;
+  }
+`;
+
+const CurrentProjectStatus = styled.div`
+  border: 1px solid #c778dd;
+  padding: 5px 10px;
+  border-radius: 3px;
+
+  display: flex;
+  align-items: center;
+  gap: 10px;
+
+  div {
+    width: 15px;
+    height: 15px;
+    background-color: #c778dd;
+  }
+  a {
+    color: var(--light-white);
+    text-decoration: none;
+  }
+`;
+
+const Quotation = styled.div`
+  text-align: center;
+  color: var(--grey);
+  margin: 5% 0px;
+
+  @media (max-width: 900px) {
+    /* Tablet and below */
+    margin: 15% 0px;
+  }
+
+  q {
+    padding: 10px 10px;
+    span {
+      font-size: 18px;
+      color: var(--yellow);
+    }
+  }
+`;
+
+const Skills = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  color: var(--grey);
+  /* align-items: center; */
+  justify-content: space-around;
+  gap: 20px;
+`;
+
+const SkillWrapper = styled.div`
+  /* width: 300px; */
+  border: 1px solid var(--grey);
+  border-radius: 8px;
+  text-align: center;
+
+  h5 {
+    padding: 15px 15px;
+    font-size: 16px;
+    font-weight: 400;
+    color: var(--light-white);
+    border-bottom: 1px solid var(--grey);
+  }
+  p {
+    padding: 0px 15px;
+  }
+`;
+
+export {
+  DescriptionAndImage,
+  Description,
+  Image,
+  Wrapper,
+  Quotation,
+  Skills,
+  SkillWrapper,
+  CurrentProjectStatus,
+};
