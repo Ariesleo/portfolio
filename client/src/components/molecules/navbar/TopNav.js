1cafe8def8d8370e5d471561a2e69ef6f43fbb1c
Murari Gupta <alexgupta2367@gmail.com>
2023-08-20 11:44:14 +0545

    added hamburger for the top nav so that it can be accessable for th mobile devices

--- client/src/components/molecules/navbar/TopNav.js
+++ client/src/components/molecules/navbar/TopNav.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useAppContext } from '../../../context/appContext.js';
-import { NavbarContainer, Logo, NavList, NavItem, NavLink } from './styles.js';
+import {
+  NavbarContainer,
+  Logo,
+  NavList,
+  NavItem,
+  NavLink,
+  Hamburger,
+  NavItem1,
+  HamburgerList,
+} from './styles.js';
 
 const TopNav = () => {
   const profileData = useAppContext();
+  const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownVisible(!isDropdownVisible);
+  };
+
   return (
     <NavbarContainer>
       <Logo>
@@ -27,6 +42,25 @@ const TopNav = () => {
           </NavItem>
         </div>
       </NavList>
+      <Hamburger onClick={toggleDropdown}>
+        <p style={{ fontWeight: '800', cursor: 'pointer' }}>
+          {isDropdownVisible ? 'X' : '☰'}
+        </p>
+        <HamburgerList isvisible={isDropdownVisible.toString()}>
+          <NavItem1>
+            <NavLink to="/">_hello</NavLink>
+          </NavItem1>
+          <NavItem1>
+            <NavLink to="/about-me">_about-me</NavLink>
+          </NavItem1>
+          <NavItem1>
+            <NavLink to="/projects">_projects</NavLink>
+          </NavItem1>
+          <NavItem1>
+            <NavLink to="/contact-me">_contact-me</NavLink>
+          </NavItem1>
+        </HamburgerList>
+      </Hamburger>
     </NavbarContainer>
   );
 };
--- client/src/components/molecules/navbar/styles.js
+++ client/src/components/molecules/navbar/styles.js
@@ -5,21 +5,37 @@ const NavbarContainer = styled.div`
   margin: 0px;
   padding: 1.1rem 1rem;
   display: flex;
+  align-items: center;
+  justify-content: space-between;
   color: var(--grey);
   background-color: var(--dark);
   border-bottom: 1px solid var(--fade-dark);
+
+  @media (max-width: 750px) {
+    /* Tablet and below */
+    padding: 0px 1rem;
+  }
 `;
 
 const Logo = styled.div`
   font-size: 1rem;
   width: 20%;
   font-weight: 450;
+  @media (max-width: 750px) {
+    /* Tablet and below */
+    width: auto;
+  }
 `;
 
 const NavList = styled.div`
   width: 100%;
   display: flex;
   justify-content: space-between;
+
+  @media (max-width: 750px) {
+    /* Tablet and below */
+    display: none;
+  }
 `;
 
 const NavItem = styled.span`
@@ -37,4 +53,44 @@ const NavLink = styled(Link)`
   }
 `;
 
-export { NavbarContainer, Logo, NavList, NavItem, NavLink };
+const Hamburger = styled.div`
+  @media (max-width: 750px) {
+    /* Tablet and below */
+    display: block;
+  }
+  @media (min-width: 751px) {
+    /* Tablet and above */
+    display: none;
+  }
+`;
+
+const NavItem1 = styled.span`
+  padding: 10px 2rem;
+  border-bottom: 1px solid var(--fade-dark);
+`;
+
+const HamburgerList = styled.div`
+  display: ${({ isvisible }) =>
+    isvisible && isvisible.toString() === 'true' ? 'flex' : 'none'};
+  flex-direction: column;
+  position: absolute;
+  top: 100%;
+  left: 0;
+  background-color: var(--dark);
+  width: 100%;
+  z-index: 1;
+
+  width: 100%;
+  /* gap: 15px; */
+`;
+
+export {
+  NavbarContainer,
+  Logo,
+  NavList,
+  NavItem,
+  NavLink,
+  Hamburger,
+  HamburgerList,
+  NavItem1,
+};
