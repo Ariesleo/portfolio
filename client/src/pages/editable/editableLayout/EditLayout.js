edf14cb30dd15469b39d7dd08487917353561eec
Murari Gupta <alexgupta2367@gmail.com>
2023-09-04 20:06:42 +0545

    checking the token and navigating to the particular pages accordingly

--- client/src/pages/editable/editableLayout/EditLayout.js
+++ client/src/pages/editable/editableLayout/EditLayout.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import TopNavBar from '../../../components/molecules/navbar/TopNavBar';
+import SingIn from '../../signIn/SingIn';
+import { useAppContext } from '../../../context/appContext';
 
 const EditLayout = () => {
-  return (
-    <>
-      <nav>
-        <TopNavBar />
-      </nav>
-      <div style={{ marginTop: '1rem' }}>
-        <Outlet />
-      </div>
-    </>
-  );
+  const { token } = useAppContext();
+  if (!token) {
+    return <SingIn />;
+  } else {
+    return (
+      <>
+        <nav>
+          <TopNavBar />
+        </nav>
+        <div style={{ marginTop: '1rem' }}>
+          <Outlet />
+        </div>
+      </>
+    );
+  }
 };
 
 export default EditLayout;
