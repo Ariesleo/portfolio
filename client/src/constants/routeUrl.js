9940757652b4492e44f6f0176da676086e3964a4
Murari Gupta <alexgupta2367@gmail.com>
2023-09-04 19:26:04 +0545

    added auth login url

--- client/src/constants/routeUrl.js
+++ client/src/constants/routeUrl.js
@@ -2,6 +2,9 @@ const ROUTEURL = {
   profile: `/api/v1/profile`,
   project: `/api/v1/projects`,
   contact: `/api/v1/contacts`,
+  auth: {
+    login: `/api/v1/auth/signin`,
+  },
 };
 
 export default ROUTEURL;
