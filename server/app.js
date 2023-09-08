c2df0af74e0a0ec7e2f9f11445be74abdf45a615
Murari Gupta <alexgupta2367@gmail.com>
2023-09-08 18:10:43 +0545

    worked on app.js

--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
 // Import required modules
 import cors from 'cors';
-import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import express from 'express';
 import config from './src/configs/config.js';
@@ -11,10 +10,6 @@ import contactRoute from './src/routes/contactsQuery.js';
 import errorHandler from './src/middleware/errorHandler.js';
 import { CommonError } from './src/lib/api/error/commonError.js';
 import { ApplicationError } from './src/lib/api/error/applicationError.js';
-
-// Get the current filename and dirname using Node.js modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 // Initialize the Express app
 const app = express();
@@ -33,9 +28,6 @@ app.use(
   express.urlencoded({ limit: config.body_parser_limit, extended: true })
 );
 
-// Serve static files from the 'build' directory
-app.use(express.static(path.join(__dirname, 'build')));
-
 // Define routes for various API endpoints
 app.use('/api/v1/contacts', contactRoute);
 app.use('/api/v1/projects', projectRoute);
@@ -43,12 +35,6 @@ app.use('/api/v1/profile', profileRoute);
 
 // Auth routes
 app.use('/api/v1/auth', authRoute);
-
-// For any GET request to any route ('/*' matches any URL), send the 'index.html' file
-// located in the 'build' directory. This is common in single-page applications (SPAs).
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
 // Handle all unhandled routes by returning a RESOURCE_NOT_FOUND error
 app.all('*', (req, res, next) => {
