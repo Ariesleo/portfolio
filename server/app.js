8cff0d756b14ff5365520c8827e51f0dd6f635d6
Murari Gupta <alexgupta2367@gmail.com>
2023-09-08 17:53:41 +0545

    rearfactored the error code

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+// Import required modules
 import cors from 'cors';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
@@ -11,47 +12,51 @@ import errorHandler from './src/middleware/errorHandler.js';
 import { CommonError } from './src/lib/api/error/commonError.js';
 import { ApplicationError } from './src/lib/api/error/applicationError.js';
 
+// Get the current filename and dirname using Node.js modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// Initializing the express app
+// Initialize the Express app
 const app = express();
 
+// Enable CORS for specified origin
 app.use(
   cors({
     origin: config.appUrl,
   })
 );
 
-// Parsing
+// Parsing middleware
 // Increase request body size limit
 app.use(express.json({ limit: config.body_parser_limit }));
 app.use(
   express.urlencoded({ limit: config.body_parser_limit, extended: true })
 );
 
-// When a GET request is made to any route ('/*' matches any URL),
-// send the 'index.html' file located in the 'build' directory.
-// This is basically used so that the route is possible in the frontendsection
+// Serve static files from the 'build' directory
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
-// Routes
+// Define routes for various API endpoints
 app.use('/api/v1/contacts', contactRoute);
 app.use('/api/v1/projects', projectRoute);
 app.use('/api/v1/profile', profileRoute);
 
-// Auth
+// Auth routes
 app.use('/api/v1/auth', authRoute);
 
-// Handling all the unhandled routes
+// For any GET request to any route ('/*' matches any URL), send the 'index.html' file
+// located in the 'build' directory. This is common in single-page applications (SPAs).
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+// Handle all unhandled routes by returning a RESOURCE_NOT_FOUND error
 app.all('*', (req, res, next) => {
   next(new ApplicationError(CommonError.RESOURCE_NOT_FOUND));
 });
 
-// Handle errors
+// Error handling middleware
 app.use(errorHandler);
 
+// Export the Express app
 export default app;
